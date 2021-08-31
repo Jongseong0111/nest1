19a11f35962f83ee54060adf0212860a98b440e4
임종성 <qkqndudnxld@gmail.com>
2021-08-31 17:40:32 +0900

    nestjs - 9 : update, delete

--- src/cats/cats.route.ts
+++ src/cats/cats.route.ts
@@ -73,4 +73,75 @@ router.post("/cats/", (req, res) => {
   }
 });
 
+//* Update cats -> Put
+
+router.put("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    const data = req.body;
+    let result;
+    Cat.forEach((cat) => {
+      if (cat.id === params.id) {
+        cat = data;
+        result = cat;
+      }
+    });
+    res.status(200).send({
+      success: true,
+      data: {
+        cat: result,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+//* Update part of cats -> Patch
+
+router.patch("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    const data = req.body;
+    let result;
+    Cat.forEach((cat) => {
+      if (cat.id === params.id) {
+        cat = { ...cat, ...data };
+        result = cat;
+      }
+    });
+    res.status(200).send({
+      success: true,
+      data: {
+        cat: result,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+//* Delete cats
+
+router.delete("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    const newCat = Cat.filter((cat) => cat.id !== params.id);
+
+    res.status(200).send({
+      success: true,
+      data: newCat,
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
 export default router;
