6079cad9a7dca6fc8bfcea09bdd1ecb6c1ef5ba4
임종성 <qkqndudnxld@gmail.com>
2021-08-31 16:50:59 +0900

    nestjs - 7

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,46 @@
 import * as express from "express";
 
 const app: express.Express = express();
-const port: number = 8000;
+import { Cat, CatType } from "./app.model";
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  let jong = { d: "dd" };
-  res.send(jong);
+const data = [1, 2, 3, 4];
+
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is middleware");
+  next();
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.get("/cats/som", (req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is some middleware");
+  next();
+});
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  console.log(req.rawHeaders[1]);
+  res.send({ cats: Cat });
+});
+app.get("/cats", (req, res) => {
+  res.send({ cats: Cat, blue: Cat[0] });
+});
+
+app.get("/cats/som", (req, res) => {
+  console.log(req.rawHeaders[1]);
+  res.send({ som: Cat[1] });
+});
+
+app.get("/cats/blue", (req, res) => {
+  console.log(req.rawHeaders[1]);
+  res.send({ blue: Cat[0] });
+});
+
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is middleware");
+  res.send({ error: "404 not found" });
+});
+
+app.listen(8000, () => {
+  console.log("server is on..");
 });
